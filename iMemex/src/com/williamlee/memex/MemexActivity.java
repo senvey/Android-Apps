516e7fea22865d1330d461a43565363aff6a1a7d
Shengwei Li <senvey@gmail.com>
2012-08-11 18:50:38 -0700

    Workable version with SQLite.

--- iMemex/src/com/williamlee/memex/MemexActivity.java
+++ iMemex/src/com/williamlee/memex/MemexActivity.java
@@ -1,37 +1,32 @@
 package com.williamlee.memex;
 
-import info.shengweili.memex.R;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Calendar;
-
-import android.app.Activity;
-import android.content.Context;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MemexActivity extends Activity {
-    
-    private static final String FILENAME = "memex.txt";
-	
-//	private boolean mExternalStorageAvailable = false;
-//	private boolean mExternalStorageWriteable = false;
-//	private String state;
+public class MemexActivity extends ListActivity {
 
+    private NotesDbAdapter mDbHelper;
+    private Cursor mNotesCursor;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        try {
+        	this.loadWords();
+        } catch (Exception ex) {
+        	
+        }
 
         Button addButton = (Button) super.findViewById(R.id.btn_add);
         addButton.setOnClickListener(new OnClickListener() {
@@ -51,60 +46,28 @@ public class MemexActivity extends Activity {
     }
     
     private void loadWords() {
-    	StringBuilder text = new StringBuilder();
-		try {
-			FileInputStream fis = super.openFileInput(FILENAME);
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-			
-			String line;
-            while ((line = br.readLine()) != null) {
-            	text.append(line);
-            }
-            fis.close();
-	        Toast.makeText(getApplication(), "Text loaded!", 1000).show();
-		} catch (FileNotFoundException e) {
-	        Toast.makeText(getApplication(), "ERROR! FileNotFoundException", 1000).show();
-		} catch (IOException e) {
-	        Toast.makeText(getApplication(), "ERROR! IOException", 1000).show();
-		}
-    	
-    	TextView tv = (TextView) super.findViewById(R.id.txt_all);
-    	tv.setText(text);
+        mNotesCursor = mDbHelper.fetchAllNotes();
+        super.startManagingCursor(mNotesCursor);
+        String[] from = new String[]{
+        		NotesDbAdapter.KEY_CONTENT,
+        		NotesDbAdapter.KEY_TIME
+		};
+        int[] to = new int[]{
+        		R.id.txt_content,
+        		R.id.txt_time
+		};
+
+        SimpleCursorAdapter notes = 
+        	    new SimpleCursorAdapter(this, R.layout.word_entry, mNotesCursor, from, to);
+    	super.setListAdapter(notes);
     }
     
     private void addWords() {
         TextView tv = (TextView) super.findViewById(R.id.txt_words);
         String words = tv.getText().toString();
-        String record = Calendar.getInstance().toString() + "," + words;
-
-        try {
-        	FileOutputStream fos = super.openFileOutput(FILENAME, Context.MODE_APPEND);
-	        fos.write(record.getBytes());
-	        fos.close();
-	        Toast.makeText(getApplication(), "Text added!", 1000).show();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-	        Toast.makeText(getApplication(), "ERROR! FileNotFoundException", 1000).show();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-	        Toast.makeText(getApplication(), "ERROR! IOException", 1000).show();
-		}
+        
+        mDbHelper.createNote(words);
+        Toast.makeText(getApplication(), "Text added!", 1000).show();
     }
     
-//    private void updateExternalStorageState() {
-//    	state = Environment.getExternalStorageState();
-//    	
-//    	if (Environment.MEDIA_MOUNTED.equals(state)) {
-//    	    // We can read and write the media
-//    	    mExternalStorageAvailable = mExternalStorageWriteable = true;
-//    	} else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-//    	    // We can only read the media
-//    	    mExternalStorageAvailable = true;
-//    	    mExternalStorageWriteable = false;
-//    	} else {
-//    	    // Something else is wrong. It may be one of many other states, but all we need
-//    	    //  to know is we can neither read nor write
-//    	    mExternalStorageAvailable = mExternalStorageWriteable = false;
-//    	}
-//    }
 }
---
+++ iMemex/src/com/williamlee/memex/NotesDbAdapter.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.williamlee.memex;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Simple notes database access helper class. Defines the basic CRUD operations
+ * for the notepad example, and gives the ability to list all notes as well as
+ * retrieve or modify a specific note.
+ * 
+ * This has been improved from the first version of this tutorial through the
+ * addition of better error handling and also using returning a Cursor instead
+ * of using a collection of inner classes (which is less scalable and not
+ * recommended).
+ */
+public class NotesDbAdapter {
+
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_CONTENT = "content";
+    public static final String KEY_TAGS = "tags";
+    public static final String KEY_TIME = "time_created";
+
+    private static final String TAG = "Memex.NotesDbAdapter";
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    private static final String DATABASE_NAME = "memex";
+    private static final String DATABASE_TABLE = "notes";
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Database creation sql statement
+     */
+    private static final String DATABASE_CREATE =
+        "CREATE TABLE " + DATABASE_TABLE +
+        		" (" + KEY_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        		KEY_CONTENT + " TEXT NOT NULL, " +
+        		KEY_TAGS + " TEXT, " +
+        		KEY_TIME + " INTEGER DEFAULT CURRENT_TIMESTAMP);";
+
+    private final Context mCtx;
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+            onCreate(db);
+        }
+    }
+
+    /**
+     * Constructor - takes the context to allow the database to be
+     * opened/created
+     * 
+     * @param ctx the Context within which to work
+     */
+    public NotesDbAdapter(Context ctx) {
+        this.mCtx = ctx;
+    }
+
+    /**
+     * Open the notes database. If it cannot be opened, try to create a new
+     * instance of the database. If it cannot be created, throw an exception to
+     * signal the failure
+     * 
+     * @return this (self reference, allowing this to be chained in an
+     *         initialization call)
+     * @throws SQLException if the database could be neither opened or created
+     */
+    public NotesDbAdapter open() throws SQLException {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        mDbHelper.close();
+    }
+
+
+    /**
+     * Create a new note using the title and body provided. If the note is
+     * successfully created return the new rowId for that note, otherwise return
+     * a -1 to indicate failure.
+     * 
+     * @param content the body of the note
+     * @return rowId or -1 if failed
+     */
+    public long createNote(String content) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_CONTENT, content);
+
+        return mDb.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    /**
+     * Delete the note with the given rowId
+     * 
+     * @param rowId id of note to delete
+     * @return true if deleted, false otherwise
+     */
+    public boolean deleteNote(long rowId) {
+        return mDb.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    /**
+     * Return a Cursor over the list of all notes in the database
+     * 
+     * @return Cursor over all notes
+     */
+    public Cursor fetchAllNotes() {
+        return mDb.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_CONTENT,
+        		KEY_TIME}, null, null, null, null, null);
+    }
+
+    /**
+     * Return a Cursor positioned at the note that matches the given rowId
+     * 
+     * @param rowId id of note to retrieve
+     * @return Cursor positioned to matching note, if found
+     * @throws SQLException if note could not be found/retrieved
+     */
+    public Cursor fetchNote(long rowId) throws SQLException {
+        Cursor mCursor =
+            mDb.query(true, DATABASE_TABLE, new String[] {KEY_ROWID, KEY_CONTENT,
+            		KEY_TIME}, KEY_ROWID + "=" + rowId, null,
+                    null, null, null, null);
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        
+        return mCursor;
+    }
+
+    /**
+     * Update the note using the details provided. The note to be updated is
+     * specified using the rowId, and it is altered to use the title and body
+     * values passed in
+     * 
+     * @param rowId id of note to update
+     * @param content value to set note body to
+     * @return true if the note was successfully updated, false otherwise
+     */
+    public boolean updateNote(long rowId, String content) {
+        ContentValues args = new ContentValues();
+        args.put(KEY_CONTENT, content);
+
+        return mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+}
